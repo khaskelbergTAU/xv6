3692893d142b5dd92714bd0dd8bbe4528d6a56d4
yuval <khaskelberg@mail.tau.ac.il>
2023-10-22 16:34:39 +0300

    added threading support

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context
+{
+  uint64 ra;
+  uint64 sp;
 
-struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
 };
+
+struct thread
+{
+  struct context context;
+  char stack[STACK_SIZE]; /* the thread's stack */
+  int state;              /* FREE, RUNNING, RUNNABLE */
+};
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
@@ -58,10 +80,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch((uint64)t, (uint64)current_thread);
   } else
     next_thread = 0;
 }
@@ -75,7 +94,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context.sp = (uint64)&t->stack[STACK_SIZE];
+  t->context.ra = (uint64)func;
 }
 
 void 
